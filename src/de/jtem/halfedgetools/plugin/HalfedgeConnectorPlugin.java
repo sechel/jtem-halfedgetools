23d66c7363dcf8a71d6c828c851baf99b190ab3e
Kristoffer Josefsson <kejace@users.noreply.github.com>
2010-01-11 15:36:20 +0000

    old version of halfedgeconnector is back
    git-svn-id: https://svn.math.tu-berlin.de/svn/jtem@618 f5b180c5-49ee-4939-b20e-b6ed35f0f7b7

--- src/de/jtem/halfedgetools/plugin/HalfedgeConnectorPlugin.java
+++ src/de/jtem/halfedgetools/plugin/HalfedgeConnectorPlugin.java
@@ -2,26 +2,15 @@ package de.jtem.halfedgetools.plugin;
 
 import static de.jreality.plugin.basic.Content.ChangeEventType.ContentChanged;
 import static java.awt.GridBagConstraints.BOTH;
-import static java.awt.GridBagConstraints.REMAINDER;
 import static javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER;
 import static javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED;
 
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridLayout;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.util.Set;
-import java.util.TreeSet;
 
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -33,33 +22,18 @@ import de.jreality.plugin.JRViewerUtility;
 import de.jreality.plugin.basic.Content;
 import de.jreality.plugin.basic.Scene;
 import de.jreality.plugin.basic.View;
-import de.jreality.plugin.basic.Content.ChangeEventType;
 import de.jreality.plugin.basic.Content.ContentChangedEvent;
 import de.jreality.plugin.basic.Content.ContentChangedListener;
 import de.jreality.scene.IndexedFaceSet;
 import de.jreality.scene.SceneGraphComponent;
 import de.jreality.scene.SceneGraphVisitor;
-import de.jreality.scene.data.Attribute;
-import de.jreality.scene.data.IntArrayArray;
-import de.jreality.scene.tool.InputSlot;
-import de.jreality.tools.DragEventTool;
-import de.jreality.tools.FaceDragEvent;
-import de.jreality.tools.FaceDragListener;
-import de.jreality.tools.LineDragEvent;
-import de.jreality.tools.LineDragListener;
-import de.jreality.tools.PointDragEvent;
-import de.jreality.tools.PointDragListener;
-import de.jreality.ui.viewerapp.FileLoaderDialog;
 import de.jtem.halfedge.Edge;
 import de.jtem.halfedge.Face;
 import de.jtem.halfedge.HalfEdgeDataStructure;
 import de.jtem.halfedge.Vertex;
-import de.jtem.halfedge.util.HalfEdgeUtils;
 import de.jtem.halfedgetools.jreality.ConverterHeds2JR;
 import de.jtem.halfedgetools.jreality.ConverterJR2Heds;
 import de.jtem.halfedgetools.jreality.adapter.Adapter;
-import de.jtem.halfedgetools.jreality.adapter.Adapter.AdapterType;
-import de.jtem.halfedgetools.jreality.adapter.standard.StandardCoordinateAdapter;
 import de.jtem.halfedgetools.jreality.node.standard.StandardEdge;
 import de.jtem.halfedgetools.jreality.node.standard.StandardFace;
 import de.jtem.halfedgetools.jreality.node.standard.StandardHDS;
@@ -71,13 +45,7 @@ import de.jtem.jrworkspace.plugin.sidecontainer.SideContainerPerspective;
 import de.jtem.jrworkspace.plugin.sidecontainer.template.ShrinkPanelPlugin;
 
 
-// TODO also store adapters
-public class HalfedgeConnectorPlugin < 
-V extends Vertex<V, E, F>,
-E extends Edge<V, E, F>, 
-F extends Face<V, E, F>,
-HDS extends HalfEdgeDataStructure<V,E,F>
->  extends ShrinkPanelPlugin implements ListSelectionListener, StatusFlavor, ActionListener {
+public class HalfedgeConnectorPlugin extends ShrinkPanelPlugin implements ListSelectionListener, StatusFlavor {
 
 	private Scene
 		scene = null;
@@ -91,55 +59,18 @@ HDS extends HalfEdgeDataStructure<V,E,F>
 		geometryList = new JList(geometryModel);
 	private JScrollPane
 		geometriesScroller = new JScrollPane(geometryList, VERTICAL_SCROLLBAR_AS_NEEDED, HORIZONTAL_SCROLLBAR_NEVER);
-	private JLabel
-		selectedNodesLabel = new JLabel("Sel. V: E: F: "),
-		hedsClassLabel = new JLabel("Class: ");
 	private GeomObject	
 		activeGeometry = null;
 	private StatusChangedListener
 		statusChangedListener = null;
-	private SelfAwareContentChangedListener
-		contentChangedListener = new SelfAwareContentChangedListener();
-	private JButton
-		rescanButton = new JButton("Rescan"),
-		loadButton = new JButton("Load"),
-		saveButton = new JButton("Save");
-		
-	private Adapter[] lastAdapters = null;
-	
-	private JFileChooser 
-		chooser = FileLoaderDialog.createFileChooser("heml", "HalfEdge Markup Language");
-	
-	private HDS cachedHEDS = null;
-	
-	private DragEventTool
-		det = new DragEventTool(InputSlot.LEFT_BUTTON);
-	
-	private int
-		selectedFace, selectedEdge, selectedVertex;
-	private Component parent;
-	
-	
-	private Set<Integer> selV = new TreeSet<Integer>();
-	private Set<Integer> selE = new TreeSet<Integer>();
-	private Set<Integer> selF = new TreeSet<Integer>();
-	private Adapter[] adapters = null;
-	
+	private ContentChangedListener
+		contentChangedListener = new MyContentChangedListener();
 	
 	public HalfedgeConnectorPlugin() {
-		this(new StandardCoordinateAdapter(AdapterType.VERTEX_ADAPTER));
-	}
-	
-	
-	public HalfedgeConnectorPlugin(Adapter... a) {
-		this.adapters  = a;
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = BOTH;
 		c.weightx = 1.0;
-		c.insets = new Insets(2, 2, 2, 2);
-		c.gridwidth = GridBagConstraints.REMAINDER;
-		shrinkPanel.add(hedsClassLabel, c);
-		shrinkPanel.add(selectedNodesLabel, c);
+		c.weighty = 1.0;
 		
 		geometryList.getSelectionModel().addListSelectionListener(this);
 		geometriesScroller.setMinimumSize(new Dimension(30, 70));
@@ -147,80 +78,66 @@ HDS extends HalfEdgeDataStructure<V,E,F>
 		geometriesPanel.setLayout(new GridLayout());
 		geometriesPanel.add(geometriesScroller);
 		geometriesPanel.setBorder(BorderFactory.createTitledBorder("Available Face Sets"));
-		
-		
-		
-		c.gridwidth = 1;
-		c.weightx = 1.0;
-		shrinkPanel.add(loadButton, c);
-		c.gridwidth = REMAINDER;
-		c.weightx = 1.0;
-		shrinkPanel.add(saveButton, c);
-		
-		c.weighty = 0.0;
-
-		shrinkPanel.add(rescanButton, c);
-		
-		c.weighty = 1.0;
 		shrinkPanel.add(geometriesPanel, c);
-		
-		rescanButton.addActionListener(this);
-		loadButton.addActionListener(this);
-		saveButton.addActionListener(this);
 	}
 	
-	public 	void actionPerformed(ActionEvent e) {
-		if (rescanButton == e.getSource()) {
-			ContentChangedEvent cce = new ContentChangedEvent(ChangeEventType.ContentChanged);
-			cce.node = contentParseRoot;
-			contentChangedListener.contentChanged(cce);
-		} 
-	}
-	
-	
+	@Override
 	public void setStatusListener(StatusChangedListener scl) {
 		statusChangedListener = scl;
 	}
 	
-	public void setSelectedVertexIndex(int i) {
-		selectedVertex = i;
+	
+	
+	public < 
+		V extends Vertex<V, E, F>,
+		E extends Edge<V, E, F>, 
+		F extends Face<V, E, F>,
+		HDS extends HalfEdgeDataStructure<V, E, F>
+	> void toHalfedge(IndexedFaceSet ifs, HDS hds, Adapter... a) {
+		ConverterJR2Heds<V, E, F> c = new ConverterJR2Heds<V, E, F>(hds.getVertexClass(), hds.getEdgeClass(), hds.getFaceClass());
+		boolean oriented = IndexedFaceSetUtility.makeConsistentOrientation(ifs);
+		if (!oriented) {
+			statusChangedListener.statusChanged("Surface is not orientable!");
+			return;
+		}
+		c.ifs2heds(ifs, hds, a);
 	}
 	
-	public void setSelectedEdgeIndex(int i) {
-		selectedEdge = i;
+	public < 
+		V extends Vertex<V, E, F>,
+		E extends Edge<V, E, F>, 
+		F extends Face<V, E, F>,
+		HDS extends HalfEdgeDataStructure<V, E, F>
+	> StandardHDS toHalfedge(IndexedFaceSet ifs, Adapter... a) {
+		ConverterJR2Heds<StandardVertex, StandardEdge, StandardFace> c = new ConverterJR2Heds<StandardVertex, StandardEdge, StandardFace>(StandardVertex.class, StandardEdge.class, StandardFace.class);
+		boolean oriented = IndexedFaceSetUtility.makeConsistentOrientation(ifs);
+		if (!oriented) {
+			statusChangedListener.statusChanged("Surface is not orientable!");
+			return null;
+		}
+		StandardHDS result = new StandardHDS();
+		c.ifs2heds(ifs, result, a);
+		return result;
 	}
 	
-	public void setSelectedFaceIndex(int i) {
-		selectedFace = i;
-	}
 	
-	public int getSelectedVertexIndex() {
-		return selectedVertex;
-	}
-	
-	public int getSelectedEdgeIndex() {
-		return selectedEdge;
-	}
-	
-	public int getSelectedFaceIndex() {
-		return selectedFace;
-	}
-
-	
-	public HDS getHalfedgeContent(HDS hds, Adapter... a) {
+	public < 
+		V extends Vertex<V, E, F>,
+		E extends Edge<V, E, F>, 
+		F extends Face<V, E, F>,
+		HDS extends HalfEdgeDataStructure<V, E, F>
+	> void getHalfedgeContent(HDS hds, Adapter... a) {
 		if (activeGeometry == null) {
-			// is there a better way to get this?
-			IndexedFaceSet ifs = (IndexedFaceSet)((GeomObject)geometryModel.get(0)).cgc.getGeometry();
+			return;
 		}
 		ConverterJR2Heds<V, E, F> c = new ConverterJR2Heds<V, E, F>(hds.getVertexClass(), hds.getEdgeClass(), hds.getFaceClass());
 		IndexedFaceSet ifs = (IndexedFaceSet)activeGeometry.cgc.getGeometry();
 		boolean oriented = IndexedFaceSetUtility.makeConsistentOrientation(ifs);
 		if (!oriented) {
 			statusChangedListener.statusChanged("Surface is not orientable!");
-			return null;
+			return;
 		}
 		c.ifs2heds(ifs, hds, a);
-		return hds;
 	}
 	
 	
@@ -240,39 +157,68 @@ HDS extends HalfEdgeDataStructure<V,E,F>
 		return result;
 	}
 	
-	public HDS getCachedHalfEdgeDataStructure(HDS hds, Adapter... a) {
-
-		if(cachedHEDS.getVertexClass() == hds.getVertexClass() &&
-				cachedHEDS.getEdgeClass() == hds.getEdgeClass() &&
-				cachedHEDS.getFaceClass() == hds.getFaceClass()) {
-			
-			return cachedHEDS;
-		} else {
-			System.err.println("cache didnt match class so returning as default hds");
-			hds = getHalfedgeContent(hds, a);
-			return hds;
-		}
-	}
-
-	public  void updateHalfedgeContent(HDS hds, boolean normals, Adapter... a) {
+	
+	public <
+		V extends Vertex<V, E, F>,
+		E extends Edge<V, E, F>,
+		F extends Face<V, E, F>,
+		HDS extends HalfEdgeDataStructure<V, E, F>
+	> void updateHalfedgeContent(HDS hds, Adapter... a) {
 		if (activeGeometry == null) {
-			SceneGraphComponent root = new SceneGraphComponent();
-			activeGeometry = new GeomObject(root);
+			return;
+		}
+		ConverterHeds2JR<V, E, F> c = new ConverterHeds2JR<V, E, F>();
+		activeGeometry.cgc.setGeometry(c.heds2ifs(hds, a));
+		contentChangedListener.contentChanged(new ContentChangedEvent(ContentChanged));
+	}
+	
+	
+	public <
+		V extends Vertex<V, E, F>,
+		E extends Edge<V, E, F>,
+		F extends Face<V, E, F>,
+		HDS extends HalfEdgeDataStructure<V, E, F>
+	> void updateHalfedgeContent(HDS hds, boolean normals, Adapter... a) {
+		if (activeGeometry == null) {
+			return;
 		}
 		ConverterHeds2JR<V, E, F> c = new ConverterHeds2JR<V, E, F>();
 		IndexedFaceSet ifs = c.heds2ifs(hds, a);
 		if (normals) {
 			IndexedFaceSetUtility.calculateAndSetNormals(ifs);
 		}
-		lastAdapters = a;
-		updateCache(hds);
 		activeGeometry.cgc.setGeometry(ifs);
-		contentChangedListener.skipNextUpdate(true);
 		contentChangedListener.contentChanged(new ContentChangedEvent(ContentChanged));
 	}
 	
 	
-	public IndexedFaceSet toIndexedFaceSet(HDS hds, boolean normals, Adapter... a) {
+	public <
+		V extends Vertex<V, E, F>,
+		E extends Edge<V, E, F>,
+		F extends Face<V, E, F>,
+		HDS extends HalfEdgeDataStructure<V, E, F>
+	> void setHalfedgeContent(HDS hds, boolean normals, Adapter... a) {
+		SceneGraphComponent root = new SceneGraphComponent();
+		activeGeometry = new GeomObject(root);
+		ConverterHeds2JR<V, E, F> c = new ConverterHeds2JR<V, E, F>();
+		IndexedFaceSet ifs = c.heds2ifs(hds, a);
+		if (normals) {
+			IndexedFaceSetUtility.calculateAndSetNormals(ifs);
+		}
+		
+		activeGeometry.cgc.setGeometry(ifs);
+		content.setContent(root);
+		contentChangedListener.contentChanged(new ContentChangedEvent(ContentChanged));
+	}
+	
+	
+	
+	public <
+		V extends Vertex<V, E, F>,
+		E extends Edge<V, E, F>,
+		F extends Face<V, E, F>,
+		HDS extends HalfEdgeDataStructure<V, E, F>
+	> IndexedFaceSet toIndexedFaceSet(HDS hds, boolean normals, Adapter... a) {
 		ConverterHeds2JR<V, E, F> c = new ConverterHeds2JR<V, E, F>();
 		IndexedFaceSet ifs = c.heds2ifs(hds, a);
 		if (normals) {
@@ -291,6 +237,7 @@ HDS extends HalfEdgeDataStructure<V,E,F>
 	}
 	
 	
+	@Override
 	public void valueChanged(ListSelectionEvent e) {
 		Object cgcObject = geometryList.getSelectedValue();
 		if (cgcObject != null) {
@@ -304,100 +251,17 @@ HDS extends HalfEdgeDataStructure<V,E,F>
 	@Override
 	public void install(Controller c) throws Exception {
 		super.install(c);
-		View viewPlugin = c.getPlugin(View.class);
-		parent = viewPlugin.getViewer().getViewingComponent();
 		content = JRViewerUtility.getContentPlugin(c);
 		content.addContentChangedListener(contentChangedListener);
 		scene = c.getPlugin(Scene.class);
 		setContentParseRoot(scene.getContentComponent());
 		contentChangedListener.contentChanged(null);
-		
-		det.setDescription("Select node");
-	    det.addFaceDragListener(new FaceDragListener() {
-	    	public void faceDragStart(FaceDragEvent e) { 
-
-	    	}
-	    	public void faceDragged(FaceDragEvent e) {
-	      
-	    	};
-	    	public void faceDragEnd(FaceDragEvent e) {
-	    		selectedFace = e.getIndex();
-	    		updateSelectedLabel();
-	    		selF.clear();
-	    		selF.add(selectedFace);
-//	    		updateIfsFromSelection();
-	    		statusChangedListener.statusChanged("Selected face: " + selectedFace);
-	    		
-	    		
-	    	} 
-	    
-	    });
-	    det.addLineDragListener(new LineDragListener() {
-	    	public void lineDragStart(LineDragEvent e) { 
-
-	    	}
-	    	public void lineDragged(LineDragEvent e) {
-	      
-	    	};
-	    	public void lineDragEnd(LineDragEvent e) {
-	    		selectedEdge = e.getIndex();
-	    		IndexedFaceSet ifs = (IndexedFaceSet)((GeomObject)geometryModel.get(0)).cgc.getGeometry();
-	    		IntArrayArray iiData=null;
-	    		int[][][] indices=new int[3][][];
-	    		iiData = (IntArrayArray)ifs.getEdgeAttributes(Attribute.INDICES);
-	    		if (iiData!=null)
-	    			indices[1]= iiData.toIntArrayArray(null);
-
-	    		// FIXME for general case
-	    		int[] vs = indices[1][selectedEdge];
-	    		int v1 = vs[0];
-	    		int v2 = vs[1];
-	    		
-	    		E ee = HalfEdgeUtils.findEdgeBetweenVertices(cachedHEDS.getVertex(v1), cachedHEDS.getVertex(v2));
-	    		selectedEdge = ee.getIndex();
-		
-	    		updateSelectedLabel();
-	    		selE.clear();
-	    		selE.add(selectedEdge);
-//	    		updateIfsFromSelection();
-	    		statusChangedListener.statusChanged("Selected edge: " + selectedEdge);
-	    		
-	    	} 
-	    
-	    });
-	    det.addPointDragListener(new PointDragListener() {
-			public void pointDragStart(PointDragEvent e) { 
-
-	    	}
-	    	public void pointDragged(PointDragEvent e) {
-	      
-	    	};
-	    	public void pointDragEnd(PointDragEvent e) {
-	    		selectedVertex = e.getIndex();
-	    		updateSelectedLabel();
-	    		selV.clear();
-	    		selV.add(selectedVertex);
-//	    		updateIfsFromSelection();
-	    		statusChangedListener.statusChanged("Selected vertex: " + selectedVertex);
-	    		
-	    		
-	    	} 
-	    
-	    });
-		    
-		content.addContentTool(det);
 	}
 	
 	
-	protected void updateSelectedLabel() {
-		selectedNodesLabel.setText("Sel. V: " + selectedVertex + " E: " + selectedEdge + " F: " + selectedFace);
-		
-	}
-
 	@Override
 	public void uninstall(Controller c) throws Exception {
 		super.uninstall(c);
-		content.removeContentTool(det);
 		content.removeContentChangedListener(contentChangedListener);
 	}
 	
@@ -425,20 +289,10 @@ HDS extends HalfEdgeDataStructure<V,E,F>
 		contentChangedListener.contentChanged(null);
 	}
 	
-	void updateCache(HDS hds) {
-		cachedHEDS = hds;
-		hedsClassLabel.setText("V: " + hds.getVertexClass().getSimpleName() + " E: " + hds.getEdgeClass().getSimpleName() + " F: " +hds.getFaceClass().getSimpleName());
-		
-	}
 	
-	public class SelfAwareContentChangedListener implements ContentChangedListener {
-		
-		private boolean ownUpdate = false;
-		
-		public void skipNextUpdate(boolean a) {
-			ownUpdate = a;
-		}
+	private class MyContentChangedListener implements ContentChangedListener {
 
+		@Override
 		public void contentChanged(ContentChangedEvent cce) {
 			geometryModel.clear(); 
 			activeGeometry = null;
@@ -465,27 +319,9 @@ HDS extends HalfEdgeDataStructure<V,E,F>
 			});
 			if (geometryModel.getSize() != 0) {
 				geometryList.setSelectedIndex(0);
-				// content updated, create heds from JRNode
-
-				HDS hds = (HDS)new StandardHDS();
-				hds = getHalfedgeContent(hds, new StandardCoordinateAdapter(AdapterType.VERTEX_ADAPTER));
-
-				
-				if(!ownUpdate ) {
-					System.err.println("Heds connector updating");
-					updateCache(hds);
-				} else {
-					System.err.println("Heds not updating because self-caused update");
-					ownUpdate = false;
-				}
 			}
-			
 		}
 		
-	}
-
-	public SelfAwareContentChangedListener getContenChangedtListener() {
-		return contentChangedListener;
 	}
 	
 }
