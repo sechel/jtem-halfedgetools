212e7f1c411fef8ae19632520d07854b49e56975
Thilo Rörig <roerig@math.tu-berlin.de>
2010-02-25 09:18:59 +0000

    git-svn-id: https://svn.math.tu-berlin.de/svn/jtem@766 f5b180c5-49ee-4939-b20e-b6ed35f0f7b7

--- src/de/jtem/halfedgetools/algorithm/subdivision/StellarLinear.java
+++ src/de/jtem/halfedgetools/algorithm/subdivision/StellarLinear.java
@@ -40,76 +40,33 @@ public class StellarLinear {
 			V v = newHeds.addNewVertex();
 			vA.set(v, fA.get(f));
 			
-			E e1 = f.getBoundaryEdge();
-			boolean positive = e1.isPositive();
-			E e2 = e1.getNextEdge();
-			E e3 = e2.getNextEdge();
-			
-			F f1 = newHeds.addNewFace();
-			F f2 = newHeds.addNewFace();
-			F f3 = newHeds.addNewFace();
-			newFaces.add(f1);
-			newFaces.add(f2);
-			newFaces.add(f3);
-			
-			E e12 = newHeds.addNewEdge();
-			E e12op = newHeds.addNewEdge();
-			E e23 = newHeds.addNewEdge();
-			E e23op = newHeds.addNewEdge();
-			E e31 = newHeds.addNewEdge();
-			E e31op = newHeds.addNewEdge();
-			
-			//triangle f1 
-			e1.linkNextEdge(e12);
-			e1.setLeftFace(f1);
-			
-			e12.setTargetVertex(v);
-			e12.linkNextEdge(e31op);
-			e12.linkPreviousEdge(e1);
-			e12.linkOppositeEdge(e12op);
-			e12.setLeftFace(f1);
-			
-			e31op.setTargetVertex(e1.getStartVertex());
-			e31op.linkNextEdge(e1);
-			e31op.linkPreviousEdge(e12);
-			e31op.linkOppositeEdge(e31);
-			e31op.setLeftFace(f1);
-			
-			//triangle f2
-			e2.linkNextEdge(e23);
-			e2.setLeftFace(f2);
-			
-			e23.setTargetVertex(v);
-			e23.linkNextEdge(e12op);
-			e23.linkPreviousEdge(e2);
-			e23.linkOppositeEdge(e23op);
-			e23.setLeftFace(f2);
-			
-			e12op.setTargetVertex(e2.getStartVertex());
-			e12op.linkNextEdge(e2);
-			e12op.linkPreviousEdge(e23);
-			e12op.linkOppositeEdge(e12);
-			e12op.setLeftFace(f2);
-			
-			//triangle f3
-			e3.linkNextEdge(e31);
-			e3.setLeftFace(f3);
-			
-			e31.setTargetVertex(v);
-			e31.linkNextEdge(e23op);
-			e31.linkPreviousEdge(e3);
-			e31.linkOppositeEdge(e31op);
-			e31.setLeftFace(f3);
-			
-			e23op.setTargetVertex(e3.getStartVertex());
-			e23op.linkNextEdge(e3);
-			e23op.linkPreviousEdge(e31);
-			e23op.linkOppositeEdge(e23);
-			e23op.setLeftFace(f3);
-			
-			e12.setIsPositive(positive);
-			e23.setIsPositive(positive);
-			e31.setIsPositive(positive);
+			E se = f.getBoundaryEdge(); //startEdge
+			E be = se;
+			E 	ne = null,
+				e1 = null,
+				e2 = null;
+			do {
+				ne = be.getNextEdge();
+				F nf = newHeds.addNewFace();
+				be.setLeftFace(nf);
+				newFaces.add(nf);
+				e2 = newHeds.addNewEdge();
+				e2.setTargetVertex(be.getStartVertex());
+				if(e1 != null) {
+					e2.linkOppositeEdge(e1);
+				}
+				e1 = newHeds.addNewEdge();
+				e1.setTargetVertex(v);
+				be.linkNextEdge(e1);
+				e1.linkNextEdge(e2);
+				e2.linkNextEdge(be);
+				
+				e1.setLeftFace(nf);
+				e2.setLeftFace(nf);
+				
+				be = ne;
+			} while (be != null);
+			e1.linkOppositeEdge(se.getPreviousEdge());
 			
 			newHeds.removeFace(f);
 			
