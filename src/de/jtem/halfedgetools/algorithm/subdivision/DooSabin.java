ffd57fba0bd51e768dba98823a4f5fd0beb2b732
Andre Heydt <heydt@math.tu-berlin.de>
2010-03-13 19:39:56 +0000

    started with doosabin, but just copy and paste from existing
    git-svn-id: https://svn.math.tu-berlin.de/svn/jtem@807 f5b180c5-49ee-4939-b20e-b6ed35f0f7b7

---
+++ src/de/jtem/halfedgetools/algorithm/subdivision/DooSabin.java
@@ -0,0 +1,132 @@
+package de.jtem.halfedgetools.algorithm.subdivision;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import de.jtem.halfedge.Edge;
+import de.jtem.halfedge.Face;
+import de.jtem.halfedge.HalfEdgeDataStructure;
+import de.jtem.halfedge.Vertex;
+import de.jtem.halfedgetools.algorithm.calculator.EdgeAverageCalculator;
+import de.jtem.halfedgetools.algorithm.calculator.FaceBarycenterCalculator;
+import de.jtem.halfedgetools.algorithm.calculator.VertexPositionCalculator;
+import de.jtem.halfedgetools.symmetry.node.SEdge;
+import de.jtem.halfedgetools.symmetry.node.SHDS;
+
+public class DooSabin {
+
+	/**
+	 * Doo-Sabin
+	 *
+	 * Generates the medial graph for the given graph
+	 * @param graph the graph
+	 * @param vClass the vertex class type of the result
+	 * @param eClass the edge class type of the result
+	 * @param fClass the face class type of the result
+	 * @param edgeTable1 this map maps edges of the graph onto edges of the result 
+	 * @return the medial
+	 * @throws SurfaceException
+	 * TODO write it as symmetric
+	 */
+	public <
+		V extends Vertex<V, E, F>,
+		E extends Edge<V, E, F>,
+		F extends Face<V, E, F>,
+		HDS extends HalfEdgeDataStructure<V,E,F>
+	>  HDS execute (
+		HDS graph,
+		HDS result,
+		Map<V, F> vertexFaceMap,
+		Map<E, V> edgeVertexMap,
+		Map<F, F> faceFaceMap,
+		Map<E, E> edgeEdgeMap1,
+		VertexPositionCalculator vA,
+		EdgeAverageCalculator eA
+	) {
+		eA.setAlpha(0.5);
+		eA.setIgnore(true);
+		vertexFaceMap.clear();
+		edgeVertexMap.clear();
+		faceFaceMap.clear();
+		edgeEdgeMap1.clear();
+		
+		HashMap<E, E> edgeEdgeMap2 = new HashMap<E, E>();
+	
+		// create faces for faces and vertices
+		for (F f : graph.getFaces()){
+			F newFace = result.addNewFace();
+			faceFaceMap.put(f, newFace);
+		}
+		for (V v : graph.getVertices()){
+			F newFace = result.addNewFace();
+			vertexFaceMap.put(v, newFace);
+		}
+		
+		// make edges and vertices
+		for (E e : graph.getEdges()){
+			V v = edgeVertexMap.get(e);
+			if (v == null) {
+				v = result.addNewVertex();
+				edgeVertexMap.put(e, v);
+				edgeVertexMap.put(e.getOppositeEdge(), v);
+				vA.set(v, eA.get(e));
+			}
+			E e1 = result.addNewEdge();
+			E e2 = result.addNewEdge();
+			e1.setTargetVertex(v);
+			e2.setTargetVertex(v);
+	
+			edgeEdgeMap1.put(e, e1);
+			edgeEdgeMap2.put(e, e2);
+		}
+		// link cycles
+		for (E e : graph.getEdges()){
+			E nextE = e.getNextEdge();
+			E e1 = edgeEdgeMap1.get(e);
+			E e2 = edgeEdgeMap2.get(e);
+			E e11 = edgeEdgeMap1.get(nextE);
+			
+			e11.linkOppositeEdge(e2);
+			e1.linkNextEdge(e11);
+			
+			F face = faceFaceMap.get(e.getLeftFace());
+			e1.setLeftFace(face);
+		}
+		// link cocycles
+		for (V v : graph.getVertices()){
+			E firstEdge = v.getIncomingEdge();
+			E actEdge = firstEdge;
+			F face = vertexFaceMap.get(v);
+			do {
+				E nextEdge = actEdge.getNextEdge().getOppositeEdge();
+				E e2 = edgeEdgeMap2.get(actEdge);
+				E e3 = edgeEdgeMap2.get(nextEdge);
+				e3.linkNextEdge(e2);
+				actEdge = nextEdge;
+				
+				e2.setLeftFace(face);
+			} while (actEdge != firstEdge);
+		}
+		return result;
+	}
+
+	public <
+		V extends Vertex<V, E, F>,
+		E extends Edge<V, E, F>,
+		F extends Face<V, E, F>,
+		HDS extends HalfEdgeDataStructure<V, E, F>
+	> Map<E, Set<E>> subdivide(
+			HDS oldHeds, 
+			HDS newHeds, 
+			VertexPositionCalculator vc,
+			EdgeAverageCalculator ec,
+			FaceBarycenterCalculator fc
+	) {
+		
+		System.err.println("doosabin-algo returns nothing yet! still under contruction");
+		return null;
+		
+	}
+	
+}
---
+++ src/de/jtem/halfedgetools/symmetry/plugin/SymmetricDooSabinPlugin.java
@@ -0,0 +1,118 @@
+/**
+This file is part of a jTEM project.
+All jTEM projects are licensed under the FreeBSD license 
+or 2-clause BSD license (see http://www.opensource.org/licenses/bsd-license.php). 
+
+Copyright (c) 2002-2010, Technische Universität Berlin, jTEM
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+-	Redistributions of source code must retain the above copyright notice, 
+	this list of conditions and the following disclaimer.
+
+-	Redistributions in binary form must reproduce the above copyright notice, 
+	this list of conditions and the following disclaimer in the documentation 
+	and/or other materials provided with the distribution.
+ 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS 
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+OF SUCH DAMAGE.
+**/
+
+package de.jtem.halfedgetools.symmetry.plugin;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import de.jtem.halfedge.Edge;
+import de.jtem.halfedge.Face;
+import de.jtem.halfedge.HalfEdgeDataStructure;
+import de.jtem.halfedge.Vertex;
+import de.jtem.halfedgetools.adapter.CalculatorException;
+import de.jtem.halfedgetools.adapter.CalculatorSet;
+import de.jtem.halfedgetools.algorithm.calculator.EdgeAverageCalculator;
+import de.jtem.halfedgetools.algorithm.calculator.FaceBarycenterCalculator;
+import de.jtem.halfedgetools.algorithm.calculator.VertexPositionCalculator;
+import de.jtem.halfedgetools.algorithm.subdivision.CatmullClark;
+import de.jtem.halfedgetools.algorithm.subdivision.DooSabin;
+import de.jtem.halfedgetools.plugin.HalfedgeAlgorithmPlugin;
+import de.jtem.halfedgetools.plugin.HalfedgeInterface;
+import de.jtem.halfedgetools.plugin.algorithm.AlgorithmCategory;
+import de.jtem.halfedgetools.symmetry.node.SEdge;
+import de.jtem.halfedgetools.symmetry.node.SFace;
+import de.jtem.halfedgetools.symmetry.node.SHDS;
+import de.jtem.halfedgetools.symmetry.node.SVertex;
+import de.jtem.halfedgetools.util.CuttingUtility.CuttingInfo;
+import de.jtem.jrworkspace.plugin.PluginInfo;
+
+public class SymmetricDooSabinPlugin extends HalfedgeAlgorithmPlugin {
+	
+	private DooSabin
+		subdivider = new DooSabin();
+	
+	@Override
+	public AlgorithmCategory getAlgorithmCategory() {
+		return AlgorithmCategory.Subdivision;
+	}
+	
+	@Override
+	public String getAlgorithmName() {
+		return "Symmetric Doo Sabin";
+	}
+	
+	
+	@Override
+	public < 
+		V extends Vertex<V, E, F>,
+		E extends Edge<V, E, F>,
+		F extends Face<V, E, F>,
+		HDS extends HalfEdgeDataStructure<V, E, F>
+	> void execute(HDS hds, CalculatorSet c, HalfedgeInterface hcp) {
+		SHDS shds = hcp.get(new SHDS());
+		SHDS result = new SHDS();
+		VertexPositionCalculator vc = c.get(shds.getVertexClass(), VertexPositionCalculator.class);
+		EdgeAverageCalculator ec = c.get(shds.getEdgeClass(), EdgeAverageCalculator.class);
+		FaceBarycenterCalculator fc = c.get(shds.getFaceClass(), FaceBarycenterCalculator.class);
+		if (vc == null || ec == null || fc == null) {
+			throw new CalculatorException("No Subdivision calculators found for " + hds);
+		}
+		Map<SEdge, Set<SEdge>> oldToDoubleNew = subdivider.subdivide(shds, result, vc, ec, fc);
+		CuttingInfo<SVertex, SEdge, SFace> symmCopy = new CuttingInfo<SVertex, SEdge, SFace>(); 
+		CuttingInfo<SVertex, SEdge, SFace> symmOld = shds.getSymmetryCycles();
+		if (symmOld != null) {
+			for(Set<SEdge> es: symmOld.paths.keySet()) {
+				Set<SEdge> newPath = new HashSet<SEdge>();
+				for(SEdge e : es) {
+					if (!oldToDoubleNew.containsKey(e)) continue;
+					for(SEdge ee : oldToDoubleNew.get(e)) {
+						newPath.add(ee);
+					}
+				}
+				symmCopy.paths.put(newPath, symmOld.paths.get(es));
+			}
+			result.setSymmetryCycles(symmCopy);
+			result.setGroup(shds.getGroup());
+		}
+		hcp.set(result);
+	}
+	
+	
+
+	@Override
+	public PluginInfo getPluginInfo() {
+		PluginInfo info = new PluginInfo("Symmetric Doo Sabin Subdivision");
+		return info;
+	}
+
+}
