fb64f9788baa906e61450af3c5893de35b4731e1
Stefan Sechelmann <sechel@math.tu-berlin.de>
2011-02-09 18:48:43 +0000

    git-svn-id: https://svn.math.tu-berlin.de/svn/jtem@1063 f5b180c5-49ee-4939-b20e-b6ed35f0f7b7

---
+++ src-test/de/jtem/halfedgetools/nurbs/IntegralCurvesTest.java
@@ -0,0 +1,15 @@
+package de.jtem.halfedgetools.nurbs;
+
+import org.junit.Test;
+
+import de.jreality.junitutils.Assert;
+
+public class IntegralCurvesTest {
+
+	@Test
+	public void testRungeKutta() throws Exception {
+		//:TODO insert test for Runge-Kutta
+		Assert.assertFalse(true);
+	}
+	
+}
