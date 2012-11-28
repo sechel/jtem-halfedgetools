e5ff44ff68da8de460ec8dada1c51184659e8cc6
Thilo Rörig <roerig@math.tu-berlin.de>
2012-11-28 08:09:28 +0000

    circumcenter moved to utility
    git-svn-id: https://svn.math.tu-berlin.de/svn/jtem@1431 f5b180c5-49ee-4939-b20e-b6ed35f0f7b7

--- src/de/jtem/halfedgetools/adapter/generic/CircumCenterAdapter.java
+++ src/de/jtem/halfedgetools/adapter/generic/CircumCenterAdapter.java
@@ -1,6 +1,5 @@
 package de.jtem.halfedgetools.adapter.generic;
 
-import de.jreality.math.Rn;
 import de.jtem.halfedge.Edge;
 import de.jtem.halfedge.Face;
 import de.jtem.halfedge.Node;
@@ -9,6 +8,7 @@ import de.jtem.halfedgetools.adapter.AbstractAdapter;
 import de.jtem.halfedgetools.adapter.AdapterSet;
 import de.jtem.halfedgetools.adapter.type.CircumCenter;
 import de.jtem.halfedgetools.adapter.type.generic.Position3d;
+import de.jtem.halfedgetools.util.GeometryUtility;
 
 @CircumCenter
 public class CircumCenterAdapter extends AbstractAdapter<double[]> {
@@ -38,27 +38,10 @@ public class CircumCenterAdapter extends AbstractAdapter<double[]> {
 			c1 = a.get(Position3d.class, v1, double[].class),
 			c2 = a.get(Position3d.class, v2, double[].class),
 			c3 = a.get(Position3d.class, v3, double[].class);
-		double[]
-		       ec1 = Rn.subtract(null, c2, c1),
-		       ec2 = Rn.subtract(null, c3, c2),
-		       ec3 = Rn.subtract(null, c1, c3);
-		double
-			alpha = 
-				Rn.euclideanNormSquared(ec2)*(-1)*Rn.innerProduct(ec1, ec3)/
-				(2*Rn.euclideanNormSquared(Rn.crossProduct(null, ec1, ec2))),
-			beta = 
-				Rn.euclideanNormSquared(ec3)*(-1)*Rn.innerProduct(ec1, ec2)/
-				(2*Rn.euclideanNormSquared(Rn.crossProduct(null, ec1, ec2))),
-			gamma = 
-				Rn.euclideanNormSquared(ec1)*(-1)*Rn.innerProduct(ec3, ec2)/
-				(2*Rn.euclideanNormSquared(Rn.crossProduct(null, ec1, ec2)));
-		double[]
-		       ac1 = Rn.times(null, alpha, c1),
-		       bc2 = Rn.times(null, beta, c2),
-		       gc3 = Rn.times(null, gamma, c3);
-		return Rn.add(null, ac1, Rn.add(null,bc2,gc3));
+		return GeometryUtility.circumCenter(c1, c2, c3);
 	}
-	
+
+
 	@Override
 	public double getPriority() {
 		return 0;
--- src/de/jtem/halfedgetools/util/GeometryUtility.java
+++ src/de/jtem/halfedgetools/util/GeometryUtility.java
@@ -27,4 +27,35 @@ public class GeometryUtility {
 		return result / n;
 	}
 	
+	public static double[] circumCircle(double[] p1, double[] p2, double[] p3) {
+		double[] circle = new double[4];
+		double[] cc = circumCenter(p1,p2,p3);
+		System.arraycopy(cc, 0, circle, 0, 3);
+		circle[3] = Rn.euclideanDistance(cc, p2);
+		return circle;
+	}
+	
+	public static double[] circumCenter(double[] c1, double[] c2, double[] c3) {
+		double[]
+		       ec1 = Rn.subtract(null, c2, c1),
+		       ec2 = Rn.subtract(null, c3, c2),
+		       ec3 = Rn.subtract(null, c1, c3);
+		double
+			alpha = 
+				Rn.euclideanNormSquared(ec2)*(-1)*Rn.innerProduct(ec1, ec3)/
+				(2*Rn.euclideanNormSquared(Rn.crossProduct(null, ec1, ec2))),
+			beta = 
+				Rn.euclideanNormSquared(ec3)*(-1)*Rn.innerProduct(ec1, ec2)/
+				(2*Rn.euclideanNormSquared(Rn.crossProduct(null, ec1, ec2))),
+			gamma = 
+				Rn.euclideanNormSquared(ec1)*(-1)*Rn.innerProduct(ec3, ec2)/
+				(2*Rn.euclideanNormSquared(Rn.crossProduct(null, ec1, ec2)));
+		double[]
+		       ac1 = Rn.times(null, alpha, c1),
+		       bc2 = Rn.times(null, beta, c2),
+		       gc3 = Rn.times(null, gamma, c3);
+		return Rn.add(null, ac1, Rn.add(null,bc2,gc3));
+	}
+	
+	
 }
