524d03035c1154534c1ca8e9403211c4932c9bfd
Stefan Sechelmann <sechel@math.tu-berlin.de>
2013-03-20 09:56:45 +0000

    an abstract halfedge listener
    git-svn-id: https://svn.math.tu-berlin.de/svn/jtem@1526 f5b180c5-49ee-4939-b20e-b6ed35f0f7b7

---
+++ src/de/jtem/halfedgetools/plugin/AbstractHalfedgeListener.java
@@ -0,0 +1,25 @@
+package de.jtem.halfedgetools.plugin;
+
+public class AbstractHalfedgeListener implements HalfedgeListener {
+
+	@Override
+	public void dataChanged(HalfedgeLayer layer) {
+	}
+
+	@Override
+	public void adaptersChanged(HalfedgeLayer layer) {
+	}
+
+	@Override
+	public void activeLayerChanged(HalfedgeLayer old, HalfedgeLayer active) {
+	}
+
+	@Override
+	public void layerCreated(HalfedgeLayer layer) {
+	}
+
+	@Override
+	public void layerRemoved(HalfedgeLayer layer) {
+	}
+
+}
